def78d313da21c4ebb2c18f4021bdb9d0c5f0a3e
michaelxuzhi <michaelxuzhi@foxmail.com>
2022-04-20 01:05:54 +0800

    新增websocket客户端间通信方法

--- src/views/wsServer.vue
+++ src/views/wsServer.vue
@@ -3,7 +3,9 @@
     <div class="son-title">websocket</div>
     <!-- <div class="son-content">子内容</div> -->
     <el-button @click="login">登录</el-button>
-    <el-button @click="sendWsMsg">sendMsg</el-button>
+    <el-button @click="createRoom('299-443210')">创建房间</el-button>
+    <el-button @click="joinRoom('299-443210')">加入房间</el-button>
+    <el-button @click="sendMsg">发送房间消息</el-button>
     <el-button @click="logout">退出</el-button>
   </div>
 </template>
@@ -16,14 +18,36 @@ export default {
   data() {
     return {
       wsServer: null,
+      roomList: [],
     };
   },
   methods: {
-    buildWs() {
-      console.log('ws');
+    buildWs(args) {
+      console.log('buildWs', args);
     },
-    sendWsMsg() {
-      console.log('send');
+    createRoom(roomID) {
+      console.log('创建room', roomID);
+      this.roomList.push(roomID);
+      this.$socket.emit('createRoom', roomID);
+    },
+    joinRoom(roomID) {
+      console.log('加入room', roomID);
+      this.roomList.push(roomID);
+      this.$socket.emit('joinRoom', roomID);
+    },
+    sendMsg() {
+      console.log('发送消息');
+      this.$socket.emit('sendMsg', {
+        roomID: this.roomList[0],
+        msg: '我是客户端消息',
+      });
+    },
+    recvMsg(data) {
+      console.log('接收到消息', data);
+      let func = data.func;
+      let args = data.args;
+      // console.log('this', data);
+      this[func](args);
     },
     login() {
       // this.logout();
@@ -48,6 +72,10 @@ export default {
     this.$socket.removeListener('welcome');
     this.$socket.on('welcome', data => {
       console.log('welcome', data);
+    });
+    this.$socket.removeListener('recvMsg');
+    this.$socket.on('recvMsg', data => {
+      this.recvMsg(data);
     });
   },
 };
