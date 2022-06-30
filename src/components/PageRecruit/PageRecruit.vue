88497ee031a869de39ffcff1ec73e782eedf2222
michaelxuzhi <michaelxuzhi@foxmail.com>
2022-07-01 00:56:59 +0800

    feat: 抽卡模块增加n连抽功能、抽卡结果展示

--- src/components/PageRecruit/PageRecruit.vue
+++ src/components/PageRecruit/PageRecruit.vue
@@ -13,10 +13,24 @@
 			>
 			<el-divider direction="vertical" />
 			<span>总次数：{{ recruitTotalCnt }}</span>
+			<el-divider direction="vertical" />
+			<span v-for="(v, k) in resShow" :key="k"
+				>{{ k }}:{{ v }}
+				<el-divider direction="vertical" />
+			</span>
 		</div>
 		<div class="btn-area">
 			<el-button type="info" @click="recruitCard(1)">单抽</el-button>
 			<el-button type="danger" @click="recruitCard(5)">五连抽</el-button>
+			<el-input
+				class="recruit-cnt-input"
+				v-model="recruitCntInput"
+				size="default"
+				placeholder="输入抽卡次数"
+				oninput="value=value.replace(/[^\d]/g,'')"
+				clearable
+			/>
+			<el-button type="warning" @click="recruitCard(+recruitCntInput)">n连抽</el-button>
 		</div>
 		<el-row :gutter="24">
 			<el-col :span="4" v-for="(item, idx) in res" :key="idx">
@@ -33,50 +47,54 @@ export default {
 	name: 'PageRecruit',
 	data() {
 		return {
-			recruitBtnCnt: 0,
+			// recruitBtnCnt: 0,
 			recruitTotalCnt: 0,
 			card_pool: '',
-			sigle_gold: 199,
-			sigle_coin: 300,
+			single_gold: 199,
+			single_coin: 300,
 			five_gold: 950,
 			five_coin: 1388,
 			consume: 0,
 			res: [],
+			resShow: {},
 			reData: [
 				{ id: 'gold', rate: 100 },
 				{ id: 'purple', rate: 300 },
 				{ id: 'blue', rate: 699 },
 			],
+			recruitCntInput: 0,
 		};
 	},
 	methods: {
-		randomRate() {},
 		recruitCard(cnt) {
 			this.res = [];
 			let errMsg = '请选择卡池';
 			let normalMsg = `点击了${cnt}连抽`;
+			let errCntMsg = '请输入正确的抽卡次数';
 			ElMessage({
 				showClose: true,
-				message: this.card_pool ? normalMsg : errMsg,
-				type: this.card_pool ? 'success' : 'error',
+				message: this.card_pool ? (cnt ? normalMsg : errCntMsg) : errMsg,
+				type: this.card_pool ? (cnt ? 'success' : 'error') : 'error',
 				effect: 'dark',
 				duration: 1000,
 			});
-			if (!this.card_pool) return;
+			if (!this.card_pool || !cnt) return;
 			for (let i = 0; i < cnt; i++) {
-				this.res.push(randomRate(this.reData));
+				let recruitRes = randomRate(this.reData);
+				this.res.push(recruitRes);
+				this.resShow[recruitRes]++ || (this.resShow[recruitRes] = 1);
 			}
-			this.recruitBtnCnt = cnt;
+			// this.recruitBtnCnt = cnt;
 			this.recruitTotalCnt += cnt;
 			this.calConsumeAndCnt(cnt);
 		},
 		calConsumeAndCnt(cnt) {
 			switch (this.card_pool) {
 				case 'pool_1':
-					this.consume += cnt == 1 ? this.sigle_gold : this.five_gold;
+					this.consume += cnt == 5 ? this.five_gold : this.single_gold * cnt;
 					break;
 				case 'pkg_1':
-					this.consume += cnt == 1 ? this.sigle_coin : this.five_coin;
+					this.consume += cnt == 5 ? this.five_coin : this.single_coin * cnt;
 					break;
 			}
 		},
@@ -84,10 +102,11 @@ export default {
 	watch: {
 		card_pool() {
 			// console.log('卡池改变', this.card_pool);
-			this.recruitBtnCnt = 0;
+			// this.recruitBtnCnt = 0;
 			this.res = [];
 			this.consume = 0;
 			this.recruitTotalCnt = 0;
+			this.resShow = {};
 		},
 	},
 };
@@ -120,5 +139,10 @@ export default {
 }
 .blue {
 	background-color: blue;
+}
+.recruit-cnt-input {
+	width: 200px;
+	margin-left: 100px;
+	margin-right: 10px;
 }
 </style>
